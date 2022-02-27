61056227e5a3d48da2bb56862c0f35a50369066e
ZlayaMorda <tarus01@mail.ru>
2022-02-27 23:30:39 +0300

    not full finished input

--- task_1/main.py
+++ task_1/main.py
@@ -10,12 +10,16 @@ def input_sentence_dict(sentence):
     input_string = input()
     sentence_num = 0
     sentence[0] = ''
+    bracket_temp = False
     for i in input_string:
-        if i != '.':
+        if i != '.' and re.match(r'\D', i) is not None:
             sentence[sentence_num] += i
-        else:
+        elif i == '.' or i == '!' or i == '?' or i == ';':
             sentence_num += 1
             sentence[sentence_num] = ''
+        elif i == '(' or i == '[' or i == '{':
+            bracket_temp = True
+            #доделать скобки и убрать остальные знаки
 
 
 input_sentence_dict(sentence_dict)
